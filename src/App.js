5c7098d869d10f84a1af9da01132d32261be31ae
Christy Perozzi <christy.perozzi@gmail.com>
2022-03-24 10:26:05 -0400

    cleaning up unused code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import Search from './Search';
 
 function App() {
 
-  // Delete this after I get the real ones 
+  // NEED TO ADD THE REST OF THEM. 
   const testActivities = ["Astronomy", "Picnicking", "Fishing", "Museum Exhibits"]
 
-  // These are from the form inputs -- move to component 
-  const [checkedState, setCheckedState] = useState(new Array(testActivities.length).fill(false));
-  const [us_state, setUsState] = useState("")
-  const [parkSearched, setParkSearched] = useState("");
-
-  // Combined search results -- keep in this component 
+  // Combined search results 
   const [allFilterResults, setAllFilterResults] = useState([])
 
   // string for searching passed up from Search
   const [searchString, setSearchString] = useState("")
 
+  // Processing selections passed up from Search and getting url string ready to fetch
   function handleSearch (activ, name, loc ) {
     let checkedActivities = testActivities.filter( (item, index) => activ[index]);
     let activitiesString = "&A=" + checkedActivities.join(",");
@@ -26,7 +22,7 @@ function App() {
     setSearchString(nameString + locationString + activitiesString )
   };
 
-  // KEEP THIS HERE AS-IS
+  // Search the DB based on search string 
   useEffect( () => {
     fetch("http://localhost:9292/parks/search/"+searchString)
       .then((r) => r.json())
@@ -35,30 +31,6 @@ function App() {
       });
   }, [searchString]);
 
-
-  //******************** */
-
-  // Handling the filters onChange (name, state, activities)
-
-  // moving to search component
-  function searchParks(e){
-    setParkSearched(e.target.value)
-  }
-
-    // moving to search component
-
-  function handleCheckboxChange(position) {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-    setCheckedState(updatedCheckedState);
-  };
-
-  // moving to search component
-  function handleLocationChange(e){
-    setUsState(e.target.value)
-  }
-
   return (
     <div className="App">
       <Search testActivities={testActivities} allFilterResults={allFilterResults} onSearchFilterChange={handleSearch} />
@@ -66,14 +38,7 @@ function App() {
       <h2> RESULTS: </h2>
         <ul>
           {allFilterResults.map( (result) => <li>{result.name} <br /> <a href={result.url}>See more</a><br /><br /></li>)}
-
         </ul>
-
-      <h1> 4. Fetch all comments for a park </h1>
-      <h1> 5. Add a comment on a park</h1>
-      <h1> 6. Delete a comment on a park</h1>
-      <h1> 7. Edit a comment on a park</h1>
-
     </div>
   );
 }
